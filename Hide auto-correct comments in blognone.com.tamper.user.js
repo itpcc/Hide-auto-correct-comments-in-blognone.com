6bdd85e8e0f1e8bda6a84b62753fe1a7a0244bb6
itpcc <id513128@gmail.com>
2014-06-11 20:44:00 +0700

    add pointer cursor for toggle link

--- Hide auto-correct comments in blognone.com.tamper.user.js
+++ Hide auto-correct comments in blognone.com.tamper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Hide auto-correct comments in blognone.com
 // @namespace  https://www.blognone.com/node/57111
-// @version    0.2.1
+// @version    0.3.2
 // @description  It will toggle to hide a comment that contains "=>" word automatically.
 // @copyright  Hide auto-correct comments in blognone.com by sQUARErOOt is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 // @author sQUARErOOt
@@ -12,17 +12,20 @@
 // 
 
 var main =  function() {
-    
-    var spellCheckComment = $(".comment-content:contains('=>'), .comment-content:contains('->'), .comment-content:contains('→'), .comment-content:contains(' >>'), .comment-content:contains(' << '), .comment").parent().parent();
-    spellCheckComment
-        .add($(".comment .comment-info a:contains('panurat2000')").parent().parent())
-        .each(function(){
-        	$(this).next('.indented').toggle();
-        	$(this).children('.comment-body').toggle();
-        	$(this).children('.comment-info')
-        	    .css("background-color","red")
-        	    .append('<span class="comment-spell-check">ระวัง! Comment นี้อาจเป็น Comment ตรวจคำ');
-        });
+	//spell check comment management
+	var spellCheckComment = $(".comment-content:contains('=>'), .comment-content:contains('->'), .comment-content:contains('→'), .comment-content:contains(' >>'), .comment-content:contains(' << '), .comment").parent().parent();
+	spellCheckComment
+		.add($(".comment .comment-info a:contains('panurat2000')").parent().parent())
+		.each(function(){
+			$(this).next('.indented').toggle();
+			$(this).children('.comment-body').toggle();
+			$(this).children('.comment-info')
+				.css("background-color","red")
+				.append('<span class="comment-spell-check-warning">ระวัง! Comment นี้อาจเป็น Comment ตรวจคำ');
+			$(this).addClass("spell-check-comment");
+		});
+	//add pointer cursor
+	$(".comment-info a[onclick]:contains('toggle')").css("cursor","pointer");
 };
 
 var script = document.createElement('script');
